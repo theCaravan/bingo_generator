9787b7a06cef0200b5b4d870db62772be2c47fe9
Hadi Alaoua <hadi.alaoua@hotmail.com>
2021-04-26 05:13:58 +0000

    Added next number script

--- next_number.py
+++ next_number.py
@@ -1,1 +1,13 @@
 import random
+
+# Parameters
+randstart = 1
+randend = 50
+nums_to_make = randend - 1
+
+raw_tbl = random.sample(range(randstart, randend), nums_to_make)
+
+for i in raw_tbl:
+    print("{}".format(i))
+    input("")
+
